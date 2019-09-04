69b3d5791382f8919d62d0b873e18018980eb817
Ani4298 <sharma.ani.4298@gmail.com>
2019-09-04 09:41:18 +0530

    Day 4: finally finished with react componenet

---
+++ src/component/app.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react'
+
+import Messenger from './messenger'
+    
+    export default class App extends Component{
+    
+    
+    
+        render(){
+    
+            return <div className="app-wrapper">
+                    <Messenger />
+                </div>
+        }
+    } 
