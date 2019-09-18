239682fb5cc376bd6445a4464d45e0c61b9f8222
Ani4298 <sharma.ani.4298@gmail.com>
2019-09-18 14:11:10 +0530

    here Day 6 Finish ...working with user enter message and some editing in objectId.js

--- src/component/messenger.js
+++ src/component/messenger.js
@@ -1,98 +1,168 @@
-import React, { Component } from "react";
-import classNames from "classname";
-import avatar from "../image/avatar.png";
-import { OrderedMap } from "immutable";
-import _ from "lodash";
+import React, { Component } from 'react'
+import classNames from "classname"
+import avatar from '../image/avatar.png'
+import { OrderedMap } from 'immutable'
+import _ from 'lodash'
+import { ObjectID } from '../helper/objectId'
 
 export default class Messenger extends Component {
+
   constructor(props) {
+
     super(props);
 
     this.state = {
-      height: window.innerHeight
-    };
+      height: window.innerHeight,
+      newMessage: 'Hello there...'
+    }
 
     this._onResize = this._onResize.bind(this);
 
     this.addTestMessages = this.addTestMessages.bind(this);
+    this.handleSend = this.handleSend.bind(this)
+    this.renderMessage = this.renderMessage.bind(this);
   }
 
+  renderMessage(message) {
+
+    return <p dangerouslySetInnerHTML={{ __html: _.get(message, 'body') }} />
+  }
+
+  handleSend() {
+
+    const { newMessage } = this.state;
+    const { store } = this.props;
+
+
+    // create new message
+
+    const messageId = new ObjectID().toString();
+    const channel = store.getActiveChannel();
+    const channelId = _.get(channel, '_id', null);
+    const currentUser = store.getCurrentUser();
+
+    const message = {
+      _id: messageId,
+      channelId: channelId,
+      body: newMessage,
+      author: _.get(currentUser, 'name', null),
+      avatar: avatar,
+      me: true,
+
+    };
+
+
+    store.addMessage(messageId, message);
+
+    this.setState({
+      newMessage: '',
+    })
+
+
+  }
   _onResize() {
+
     this.setState({
       height: window.innerHeight
     });
   }
 
   componentDidMount() {
-    window.addEventListener("resize", this._onResize);
+
+
+
+    window.addEventListener('resize', this._onResize);
+
 
     this.addTestMessages();
+
+
   }
 
   addTestMessages() {
+
     const { store } = this.props;
+
+
+
+
+
 
     // create test messages
     for (let i = 0; i < 100; i++) {
+
       let isMe = false;
 
+
       if (i % 3 === 0) {
-        isMe = true;
+        isMe = true
       }
       const newMsg = {
         _id: `${i}`,
         author: `Author ${i}`,
         body: `The body of message ${i}`,
         avatar: avatar,
-        me: isMe
-      };
+        me: isMe,
+      }
 
       //console.log(typeof i);
       store.addMessage(i, newMsg);
       // i need update my component and re-render it now because new messages added.
+
     }
+
 
     // create some test channels
 
     for (let c = 0; c < 10; c++) {
+
       let newChannel = {
         _id: `${c}`,
         title: `Channel title ${c}`,
         lastMessage: `Hey there here...${c}`,
         members: new OrderedMap({
-          "2": true,
-          "3": true,
-          "1": true
+          '2': true,
+          '3': true,
+          '1': true,
         }),
-        messages: new OrderedMap()
-      };
+        messages: new OrderedMap(),
+      }
 
       const msgId = `${c}`;
-      const moreMsgId = `${c + 1}`;
+      const moreMsgId = `${c + 1}`
       newChannel.messages = newChannel.messages.set(msgId, true);
       newChannel.messages = newChannel.messages.set(moreMsgId, true);
 
       store.addChannel(c, newChannel);
     }
+
+
+
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this._onResize);
+
+    window.removeEventListener('resize', this._onResize)
+
   }
 
   render() {
+
     const { store } = this.props;
 
     const { height } = this.state;
 
     const style = {
-      height: height
+      height: height,
     };
+
 
     const activeChannel = store.getActiveChannel();
     const messages = store.getMessagesFromChannel(activeChannel); //store.getMessages();
     const channels = store.getChannels();
     const members = store.getMembersFromChannel(activeChannel);
+
+
 
     return (
       <div style={style} className="app-messenger">
@@ -102,30 +172,30 @@ export default class Messenger extends Component {
               <button>New message</button>
             </div>
           </div>
-          <div className="content">
-            <h2>Title</h2>
-          </div>
+          <div className="content"><h2>{_.get(activeChannel, 'title', '')}</h2></div>
           <div className="right">
+
             <div className="user-bar">
               <div className="profile-name">Aniket</div>
-              <div className="profile-image">
-                <img src={avatar} alt="" />
-              </div>
+              <div className="profile-image"><img src={avatar} alt="" /></div>
+
             </div>
+
           </div>
         </div>
         <div className="main">
           <div className="sidebar-left">
+
             <div className="chanels">
+
               {channels.map((channel, key) => {
+
                 return (
-                  <div
-                    onClick={key => {
-                      store.setActiveChannelId(channel._id);
-                    }}
-                    key={channel._id}
-                    className="chanel"
-                  >
+                  <div onClick={(key) => {
+
+                    store.setActiveChannelId(channel._id);
+
+                  }} key={channel._id} className={classNames('chanel', { 'active': _.get(activeChannel, '_id') === _.get(channel, '_id', null) })}>
                     <div className="user-image">
                       <img src={avatar} alt="" />
                     </div>
@@ -133,48 +203,80 @@ export default class Messenger extends Component {
                       <h2>{channel.title}</h2>
                       <p>{channel.lastMessage}</p>
                     </div>
+
                   </div>
-                );
+                )
+
               })}
+
+
+
+
+
+
             </div>
           </div>
           <div className="content">
             <div className="messages">
+
               {messages.map((message, index) => {
+
+
+
                 return (
-                  <div
-                    key={index}
-                    className={classNames("message", { me: message.me })}
-                  >
+                  <div key={index} className={classNames('message', { 'me': message.me })}>
                     <div className="message-user-image">
                       <img src={message.avatar} alt="" />
                     </div>
                     <div className="message-body">
-                      <div className="message-author">
-                        {message.me ? "You " : message.author} says:
-                      </div>
+                      <div className="message-author">{message.me ? 'You ' : message.author} says:</div>
                       <div className="message-text">
-                        <p>{message.body}</p>
+                        {this.renderMessage(message)}
                       </div>
                     </div>
                   </div>
-                );
+                )
+
+
               })}
+
+
+
+
+
             </div>
 
             <div className="messenger-input">
+
               <div className="text-input">
-                <textarea placeholder="Write your messsage..." />
+                <textarea onKeyUp={(event) => {
+
+
+                  if (event.key === 'Enter' && !event.shiftKey) {
+                    this.handleSend();
+                  }
+
+
+                }} onChange={(event) => {
+
+
+
+                  this.setState({ newMessage: _.get(event, 'target.value') });
+
+                }} value={this.state.newMessage} placeholder="Write your messsage..." />
               </div>
               <div className="actions">
-                <button className="send">Send</button>
+                <button onClick={this.handleSend} className="send">Send</button>
               </div>
             </div>
           </div>
           <div className="sidebar-right">
             <h2 className="title">Members</h2>
             <div className="members">
+
               {members.map((member, key) => {
+
+
                 return (
                   <div key={key} className="member">
                     <div className="user-image">
@@ -182,15 +284,23 @@ export default class Messenger extends Component {
                     </div>
                     <div className="member-info">
                       <h2>{member.name}</h2>
-                      <p>Joined:3 day ago</p>
+                      <p>Joined: 3 days ago.</p>
                     </div>
+
                   </div>
-                );
+                )
+
               })}
+
+
+
+
+
+
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,51 +1,81 @@
-import { OrderedMap } from "immutable";
+import { OrderedMap } from 'immutable'
+import _ from 'lodash'
 
 const users = OrderedMap({
-  "1": { _id: "1", name: "aniket", created: new Date() },
-  "2": { _id: "2", name: "Sharma", created: new Date() },
-  "3": { _id: "3", name: "Kuamr", created: new Date() }
-});
+  '1': { _id: '1', name: "ANIKET", created: new Date() },
+  '2': { _id: '2', name: "ANI", created: new Date() },
+  '3': { _id: '3', name: "SharmaJi", created: new Date() },
+})
 
 export default class Store {
   constructor(appComponent) {
+
     this.app = appComponent;
     this.messages = new OrderedMap();
     this.channels = new OrderedMap();
     this.activeChannelId = null;
 
     this.user = {
-      _id: 0,
-      name: "Toan",
-      created: new Date()
-    };
+      _id: '1',
+      name: 'Toan',
+      created: new Date(),
+    }
+
+  }
+
+
+  getCurrentUser() {
+
+    return this.user;
   }
 
   setActiveChannelId(id) {
+
     this.activeChannelId = id;
     this.update();
+
   }
   getActiveChannel() {
-    const channel = this.activeChannelId
-      ? this.channels.get(this.activeChannelId)
-      : this.channels.first();
-    return channel;
-  }
-  addMessage(index, message = {}) {
-    this.messages = this.messages.set(`${index}`, message);
 
+    const channel = this.activeChannelId ? this.channels.get(this.activeChannelId) : this.channels.first();
+    return channel;
+
+  }
+  addMessage(id, message = {}) {
+
+    this.messages = this.messages.set(`${id}`, message);
+
+    // let's add new message id to current channel->messages.
+
+    const channelId = _.get(message, 'channelId');
+    if (channelId) {
+
+      const channel = this.channels.get(channelId);
+
+      channel.messages = channel.messages.set(id, true);
+      this.channels = this.channels.set(channelId, channel);
+    }
     this.update();
+
   }
   getMessages() {
+
     return this.messages.valueSeq();
   }
 
   getMessagesFromChannel(channel) {
+
     let messages = [];
+
+
 
     if (channel) {
       channel.messages.map((value, key) => {
+
+
         const message = this.messages.get(key);
         messages.push(message);
+
       });
     }
 
@@ -53,15 +83,19 @@ export default class Store {
   }
 
   getMembersFromChannel(channel) {
+
     const members = [];
 
     if (channel) {
+
       channel.members.map((value, key) => {
-        console.log("Debug key", key);
+
+
 
         const member = users.get(key);
 
         members.push(member);
+
       });
     }
 
@@ -73,10 +107,12 @@ export default class Store {
     this.update();
   }
   getChannels() {
+
     return this.channels.valueSeq();
   }
 
   update() {
-    this.app.forceUpdate();
+
+    this.app.forceUpdate()
   }
 }
